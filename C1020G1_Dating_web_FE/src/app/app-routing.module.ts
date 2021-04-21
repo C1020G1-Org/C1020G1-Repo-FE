9ade00786a0d80271d6b345f85f718955369cb75
unknown <dunghoanh1996@gmail.com>
2021-04-21 21:00:20 +0700

    update

--- C1020G1_Dating_web_FE/src/app/app-routing.module.ts
+++ C1020G1_Dating_web_FE/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TimelineComponent} from "./wall/timeline/timeline.component";
+import {InformationComponent} from "./wall/information/information.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'timeline' , component: TimelineComponent},
+  {path: 'info' , component: InformationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
