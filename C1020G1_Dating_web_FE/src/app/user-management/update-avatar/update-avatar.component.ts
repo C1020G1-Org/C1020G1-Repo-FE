e420ec82f75db05fc0982eeed772426ddb0eaaa1
huuhan2507@gmail.com <you@example.com>
2021-04-21 20:35:12 +0700

    update

---
+++ C1020G1_Dating_web_FE/src/app/user-management/update-avatar/update-avatar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-avatar',
+  templateUrl: './update-avatar.component.html',
+  styleUrls: ['./update-avatar.component.css']
+})
+export class UpdateAvatarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- C1020G1_Dating_web_FE/src/app/user-management/user-management.module.ts
+++ C1020G1_Dating_web_FE/src/app/user-management/user-management.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UpdateAvatarComponent } from './update-avatar/update-avatar.component';
+import { UpdateStatusComponent } from './update-status/update-status.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [UpdateAvatarComponent, UpdateStatusComponent, ChangePasswordComponent],
+    exports: [
+        ChangePasswordComponent
+    ],
+    imports: [
+        CommonModule
+    ]
 })
 export class UserManagementModule { }
